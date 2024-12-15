ee9184901d6942437a8ddb0762f6bf73570473b3
Ahsan-Asim <ahsanasim252@gmail.com>
2024-12-15 20:00:52 +0500

    Add link to model

--- shipping_backend/tests/test_app.py
+++ shipping_backend/tests/test_app.py
@@ -1,8 +1,8 @@
 # tests/test_app.py
-
 import json
 import pytest
-from ..app import app  # Use relative import to access app.py
+from app import app  # Absolute import
+
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
@@ -10,7 +10,7 @@ def client():
         yield client
 
 def test_check_url(client):
-    response = client.post('/check_url', json={'text': 'http://example.com/phishing'})
+    response = client.post('/check_url', json={'text': 'http://example.com/phishing'})  # Use relative URL
     assert response.status_code == 200
     data = json.loads(response.data)
     assert 'prediction' in data
